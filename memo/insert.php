b6bd35ee626afe8d1a389ff094259d4fc2de0f19
jhc0510 <134457840+jhc0510@users.noreply.github.com>
2023-08-03 21:35:27 +0900

    Add files via upload

--- memo/insert.php
+++ memo/insert.php
@@ -7,11 +7,11 @@ if (isset($_SESSION['userid']))
         $userlevel = $_SESSION['userlevel'];
 }
 $table = "memo";
-$content = $_POST['content'];
+$content = isset($_POST['content']) ? htmlspecialchars($_POST['content'], ENT_QUOTES, 'UTF-8') : '';
 if (isset($_GET['mode'])) {
 $mode = $_GET['mode'];
-$find = $_POST['find'];
-$search = $_POST['search'];
+$find = isset($_POST['find']) ? $_POST['find'] : '';
+$search = isset($_POST['search']) ? $_POST['search'] : '';
 }?>
 <meta charset="utf-8">
 <?
@@ -39,24 +39,26 @@ $search = $_POST['search'];
 
 	include "../lib/dbconn.php";       // dconn.php 파일을 불러옴
 
+	// 데이터베이스에 삽입하기 전에 데이터 처리
+	$name = htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
+	$nick = htmlspecialchars($nick, ENT_QUOTES, 'UTF-8');
+	$content = htmlspecialchars($content, ENT_QUOTES, 'UTF-8');
+	
 	$sql = "select * from member where id='$userid'";
 	$result = $connect->query($sql);
-
+	
 	$row = $result->fetch_array(MYSQLI_ASSOC);
-	$name = $row['name'];
-	$nick = $row['nick'];
-
+	$name = htmlspecialchars($row['name'], ENT_QUOTES, 'UTF-8');
+	$nick = htmlspecialchars($row['nick'], ENT_QUOTES, 'UTF-8');
+	
 	$sql = "insert into memo (id, name, nick, content, regist_day) ";
 	$sql .= "values('$userid', '$name', '$nick', '$content', '$regist_day')";
-
+	
 	$result = $connect->query($sql);
 	$connect->close();                // DB 연결 끊기
-
-	echo "
-	   <script>
-	    location.href = 'memo.php';
-	   </script>
-	";
-?>
-
+	
+	echo "<script>
+			location.href = 'memo.php';
+		  </script>";
+	?>
   
