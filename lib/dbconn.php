8e94d27072af27958b3960a926a1060b620debe2
qmffodlej1 <140581561+qmffodlej1@users.noreply.github.com>
2023-08-04 10:04:21 +0900

    Add files via upload

--- lib/dbconn.php
+++ lib/dbconn.php
@@ -4,11 +4,10 @@ $user = "paddlers"; // 데이터베이스 사용자명
 $password = "qhdks12"; // 데이터베이스 비밀번호
 $db = "paddlers"; // 데이터베이스 이름
 // $page = 1;
-
-$connect = new mysqli($host, $user, $password, $db);
+$pdo = new PDO("mysql:host=$host;dbname=$db", $user, $password);
 
 // 데이터베이스 연결 오류 체크
-if ($connect->connect_error) {
-    die("데이터베이스 연결에 실패했습니다: " . $connect->connect_error);
-}
+// if ($pdo->connect_error) {
+//     die("데이터베이스 연결에 실패했습니다: " . $connect->connect_error);
+// }
 ?>
