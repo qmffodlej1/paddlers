175256c2c7a68929b6ecfe2512fb088441ccce10
qmffodlej1 <140581561+qmffodlej1@users.noreply.github.com>
2023-08-04 12:01:48 +0900

    Add files via upload

--- member/insert.php
+++ member/insert.php
@@ -6,24 +6,23 @@
 	$hp1 = $_POST['hp1'];
 	$hp2 = $_POST['hp2'];
 	$hp3 = $_POST['hp3'];
-	$email1 = $_POST['email1'];
-	$email2 = $_POST['email2'];
 ?>
 <meta charset="utf-8">
 <?
    $hp = $hp1."-".$hp2."-".$hp3;
-   $email = $email1."@".$email2;
 
    $regist_day = date("Y-m-d (H:i)");  // 현재의 '년-월-일-시-분'을 저장
    $ip = $_SERVER['REMOTE_ADDR'];         // 방문자의 IP 주소를 저장
 
    include "../lib/dbconn.php";       // dconn.php 파일을 불러옴
 
-   $sql = "select * from member where id='$id'";
-   $result = $connect->query($sql);
-   $exist_id = $result->num_rows;
+  $data = $pdo->prepare('SELECT * FROM member WHERE id = (:id) LIMIT 1;');
+  $data->bindParam(':id', $id, PDO::PARAM_STR );
+  $data->execute();
+  $row = $data->fetch();
 
-   if($exist_id) {
+
+   if(isset($row['id'])) {
      echo("
            <script>
              window.alert('해당 아이디가 존재합니다.')
@@ -35,11 +34,17 @@
    else
    {            // 레코드 삽입 명령을 $sql에 입력
 	    $sql = "insert into member(id, pass, name, nick, hp, email, regist_day, level) ";
-		  $sql .= "values('$id', '$pass', '$name', '$nick', '$hp', '$email', '$regist_day', 9)";
-		  $connect->query($sql);
+      $data = $pdo->prepare('INSERT INTO member (id, pass, name, nick, hp, regist_day, level) values ((:id), (:pass), (:nme), (:nick), (:hp), (:regist_day), (:lv));');
+      $data->bindParam(':id', $id, PDO::PARAM_STR);
+      $data->bindParam(':pass', $pass, PDO::PARAM_STR);
+      $data->bindParam(':nme', $name, PDO::PARAM_STR);
+      $data->bindParam(':nick', $nick, PDO::PARAM_STR);
+      $data->bindParam(':hp', $hp, PDO::PARAM_STR);
+      $data->bindParam(':regist_day', $regist_day, PDO::PARAM_STR);
+      $data->bindValue(':lv', 9, PDO::PARAM_INT);
+		  $data->execute();
       
    }
-   $connect->close();
    echo "
    <script>
    location.href = '../login/login_form.php';
