2557818979b8b5609864565076a9961afc44f84e
qmffodlej1 <140581561+qmffodlej1@users.noreply.github.com>
2023-08-04 09:16:48 +0900

    Add files via upload

--- login/login.php
+++ login/login.php
@@ -1,13 +1,11 @@
 <?
+include "../lib/dbconn.php";
 session_start();
-  $id = $_POST[ 'id' ];
-  $id = stripslashes( $id );
-  $id = ((isset($GLOBALS["___mysqli_ston"]) && is_object($GLOBALS["___mysqli_ston"])) ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"],  $id ) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
-  $pass = $_POST[ 'pass' ];
-	$pass = stripslashes( $pass );
-	$pass = ((isset($GLOBALS["___mysqli_ston"]) && is_object($GLOBALS["___mysqli_ston"])) ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"],  $pass ) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
-	$table = "member";
-  // 받은 변수들 문자열로 치환
+$id = $_POST['id'];
+$pass = $_POST['pass'];
+$id = stripslashes($id);
+$pass = stripslashes($pass);
+$table = "member";
 
 	$total_failed_login = 3;
 	$lockout_time       = 15;
@@ -16,9 +14,10 @@ session_start();
 ?>
 <meta charset="utf-8">
 <?
-	$data = $db->prepare( 'SELECT failed_login, last_login FROM memeber WHERE id = (:id) LIMIT 1;' );
-	$data->bindParam( ':id', $id, PDO::PARAM_STR );
-	$data->execute();
+$data = $pdo->prepare( 'SELECT failed_login, last_login FROM member WHERE id = (:id) LIMIT 1;' );
+$data->bindParam( ':id', $id, PDO::PARAM_STR );
+$data->execute();
+
 	$row = $data->fetch();  //로그인 실패 횟수 확인문
 
   if( ( $data->rowCount() == 1 ) && ( $row[ 'failed_login' ] >= $total_failed_login ) )  { //3회 이상 실패한다면
@@ -33,17 +32,17 @@ session_start();
 		$failed_login = $row[ 'failed_login' ];
 		$last_login   = $row[ 'last_login' ];
 		if( $failed_login >= $total_failed_login ) {
-			$html .= "<p><em>Warning</em>: 3회 이상 로그인 실패 하셨습니다. </p>";
+			$html .= "<p><em>Warning</em>: 3회 이n상 로그인 실패 하셨습니다. </p>";
 			$html .= "<p>실패 횟수: <em>{$failed_login}</em>.<br />마지막 접속 시간: <em>{$last_login}</em>.</p>";
 		}
-		$data = $db->prepare( 'UPDATE users SET failed_login = "0" WHERE user = (:user) LIMIT 1;' );
-		$data->bindParam( ':user', $user, PDO::PARAM_STR );
+		$data = $pdo->prepare( 'UPDATE member SET failed_login = "0" WHERE id = (:id) LIMIT 1;' );
+		$data->bindParam( ':id', $id, PDO::PARAM_STR );
 		$data->execute();  //실패 횟수 초기화
 	} 
   else {
 		sleep( rand( 2, 4 ) ); // 로그인 실패시 sleep 시간 지정
-		$data = $db->prepare( 'UPDATE users SET failed_login = (failed_login + 1) WHERE user = (:user) LIMIT 1;' );
-		$data->bindParam( ':user', $user, PDO::PARAM_STR );
+		$data = $pdo->prepare( 'UPDATE member SET failed_login = (failed_login + 1) WHERE id = (:id) LIMIT 1;' );
+		$data->bindParam( ':id', $id, PDO::PARAM_STR );
 		$data->execute(); // 실패시 실패 카운트 증가
 	}
 
@@ -67,12 +66,11 @@ if(!$pass) {
   ");
   exit;
 }
-include "../lib/dbconn.php";
-   $sql = "select * from $table where id='$id'";
-  $result = $connect->query($sql);
-  $num_match = $result->num_rows; //SQL 쿼리문
-
-  if(!$num_match) {
+$data = $pdo->prepare('SELECT * from member WHERE id = (:id) LIMIT 1;');
+$data->bindParam( ':id', $id, PDO::PARAM_STR );
+$data->execute(); // 실패시 실패 카운트 증가
+$row = $data->fetch();
+  if(!$row['id']) {
     echo("
       <script>
         window.alert('등록되지 않은 아이디입니다.')
@@ -81,8 +79,7 @@ include "../lib/dbconn.php";
     ");
   }
   else {
-    $row = $result->fetch_array(MYSQLI_ASSOC);
-    $db_pass = $row['pass'];
+    $db_pass = $row[ 'pass' ];
       if($pass != $db_pass) {
         echo("
           <script>
@@ -101,8 +98,8 @@ include "../lib/dbconn.php";
         $_SESSION['username'] = $username;
         $_SESSION['usernick'] = $usernick;
         $_SESSION['userlevel'] = $userlevel;
-        $data = $db->prepare( 'UPDATE users SET last_login = now() WHERE id = (:id) LIMIT 1;' );
-        $data->bindParam( ':user', $id, PDO::PARAM_STR );
+        $data = $pdo->prepare( 'UPDATE member SET last_login = now() WHERE id = (:id) LIMIT 1;' );
+        $data->bindParam( ':id', $id, PDO::PARAM_STR );
         $data->execute();      
           echo("
             <script>
