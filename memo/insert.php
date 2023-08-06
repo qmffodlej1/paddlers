fc30af0d0f2742d5fdbfa0d2386b3ae3cf1b7c32
jhc0510 <134457840+jhc0510@users.noreply.github.com>
2023-08-06 13:28:20 +0900

    Add files via upload

--- memo/insert.php
+++ memo/insert.php
@@ -1,5 +1,4 @@
 <? session_start(); 
-?><meta charset="UTF-8"><?
 if (isset($_SESSION['userid'])) 
 {
         $userid = $_SESSION['userid'];
@@ -25,6 +24,7 @@ $search = isset($_POST['search']) ? $_POST['search'] : '';
 		");
 		exit;
 	}
+
 	if(!$content) {
 		echo("
 	   <script>
@@ -34,30 +34,30 @@ $search = isset($_POST['search']) ? $_POST['search'] : '';
 		");
 	 exit;
 	}
+
 	$regist_day = date("Y-m-d (H:i)");  // 현재의 '년-월-일-시-분'을 저장
 
 	include "../lib/dbconn.php";       // dconn.php 파일을 불러옴
 
 	// 데이터베이스에 삽입하기 전에 데이터 처리
-	$name = htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
-	$nick = htmlspecialchars($nick, ENT_QUOTES, 'UTF-8');
+	// $name = htmlspecialchars($name, ENT_QUOTES, 'UTF-8');
+	// $nick = htmlspecialchars($nick, ENT_QUOTES, 'UTF-8');
 	$content = htmlspecialchars($content, ENT_QUOTES, 'UTF-8');
-	$data = $pdo->prepare('SELECT * FROM member WHERE id = (:id);');
-	$data->bindParam(':id', $userid, PDO::PARAM_STR);
-	$data->execute();
-	$row = $data->fetch(PDO::FETCH_ASSOC);
+	
+	$sql = "select * from member where id='$userid'";
+	$result = $connect->query($sql);
+	
+	$row = $result->fetch_array(MYSQLI_ASSOC);
 	$name = htmlspecialchars($row['name'], ENT_QUOTES, 'UTF-8');
 	$nick = htmlspecialchars($row['nick'], ENT_QUOTES, 'UTF-8');
-
-	$data_insert = $pdo->prepare('INSERT INTO memo (id, name, nick, content, regist_day) VALUES (:userid, :nme, :nick, :content, :regist_day);');
-	$data_insert->bindParam(':userid',$userid,PDO::PARAM_STR);
-	$data_insert->bindParam(':nme',$name,PDO::PARAM_STR);
-	$data_insert->bindParam(':nick',$nick,PDO::PARAM_STR);
-	$data_insert->bindParam(':content',$content,PDO::PARAM_STR);
-	$data_insert->bindParam(':regist_day',$regist_day,PDO::PARAM_STR);
-	$data_insert->execute();
+	
+	$sql = "insert into memo (id, name, nick, content, regist_day) ";
+	$sql .= "values('$userid', '$name', '$nick', '$content', '$regist_day')";
+	
+	$result = $connect->query($sql);
+	$connect->close();                // DB 연결 끊기
+	
 	echo "<script>
 			location.href = 'memo.php';
 		  </script>";
 	?>
-  
