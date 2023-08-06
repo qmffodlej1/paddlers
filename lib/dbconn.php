916ba3492fc2cf01ab05fde2bcf7e3e0e6e00c22
jhc0510 <134457840+jhc0510@users.noreply.github.com>
2023-08-06 13:28:44 +0900

    Update dbconn.php

--- lib/dbconn.php
+++ lib/dbconn.php
@@ -5,7 +5,7 @@ $password = "qhdks12"; // 데이터베이스 비밀번호
 $db = "paddlers"; // 데이터베이스 이름
 // $page = 1;
 $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $password);
-
+$connect = new mysqli($host, $user, $password, $db);
 // 데이터베이스 연결 오류 체크
 // if ($pdo->connect_error) {
 //     die("데이터베이스 연결에 실패했습니다: " . $connect->connect_error);
