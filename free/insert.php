7da028086975a8ad24ac5097a0cb4d70131a5b58
KeoDaRan <134457919+KeoDaRan@users.noreply.github.com>
2023-08-07 16:19:23 +0900

    Add files via upload

--- free/insert.php
+++ free/insert.php
@@ -147,6 +147,21 @@ if (isset($_SESSION['userid']))
 
  	if ($mode=="modify")
 	{
+		$sql = "select * from free where num=$num";
+		$result = $connect->query($sql);
+		$row = $result->fetch_array(MYSQLI_ASSOC);
+		$item_id = $row['id'];
+		
+		if(!$userid != $item_id) {
+			echo("
+			<script>
+			 window.alert('글쓴이 정보가 일치하지 안습니다.')
+			 history.go(-2)
+		   </script>
+			");
+			exit;
+		}
+
 		if(isset($_POST['del_file']) && empty($_POST['del_file'])) {
 		$num_checked = count($_POST['del_file']);
 		$position = $_POST['del_file'];
