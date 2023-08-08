5397ea8ff6b9033d80c66d666f62ab2f7d97558f
KeoDaRan <134457919+KeoDaRan@users.noreply.github.com>
2023-08-08 15:21:07 +0900

    Update delete.php

--- free/delete.php
+++ free/delete.php
@@ -16,6 +16,7 @@
 			$userlevel = $_SESSION['userlevel'];
 	}
 
+	if(@$userid != 'admin') {
 	if(@$userid != $item_id) {
 		echo("
 		<script>
@@ -24,7 +25,7 @@
 	   </script>
 		");
 		exit;
-	}
+	}}
    $sql = "select * from $table where num = '$num'";
    $result = $connect->query($sql);
 
